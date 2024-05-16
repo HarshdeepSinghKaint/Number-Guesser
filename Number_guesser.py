b0fc20dde19e5e39c1ae29df2ab8f0381437db22
HarshdeepSinghKaint <harshdeepsinghkaint@gmail.com>
2024-05-16 23:55:11 +0530

    2nd commit

--- Number_guesser.py
+++ Number_guesser.py
@@ -1,9 +1,18 @@
 import random 
-number=random.randrange(-1,11)
+number=random.randrange(11)
 
 print("Guess any number between 0 to 10 and Test your luck")
 user_number=input("Enter your number : ")
 
+if user_number.isdigit():
+    user_number=int(user_number)
+    if user_number<0:
+        print("Please type a number greater than zero next time")
+        quit()
+else:
+    print("please type a number next time")
+    quit()
+    
 if number== user_number:
     print("YAY , you guessed it right")
 else:
